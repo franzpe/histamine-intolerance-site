f437f42b16d0515199128cb8252c1dfd09aae208
franzp <pobocekfrantisek@gmail.com>
2018-12-27 22:59:05 +0100

    Client - rating food behaviour optimized

--- client/src/foods/Food.js
+++ client/src/foods/Food.js
@@ -1,7 +1,7 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Suspense, useState } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, TableRow, TableCell, Typography } from '@material-ui/core';
-import { yellow, green } from '@material-ui/core/colors';
+import { withStyles, TableRow, TableCell, Typography, CircularProgress } from '@material-ui/core';
+import { yellow, green, grey } from '@material-ui/core/colors';
 import classNames from 'classnames';
 import DownIcon from '@material-ui/icons/ArrowDropDown';
 import UpIcon from '@material-ui/icons/ArrowDropUp';
@@ -11,6 +11,9 @@ import { AUTHENTICATION_QUERY } from '_queries/client/userQueries';
 import gql from 'graphql-tag';
 
 const styles = theme => ({
+  ratingCell: {
+    position: 'relative'
+  },
   rating: {
     display: 'inline-block'
   },
@@ -24,10 +27,23 @@ const styles = theme => ({
     color: green[700]
   },
   ratingButton: {
+    position: 'absolute',
     cursor: 'pointer'
   },
+  upRatingButton: {
+    left: '0px',
+    top: '12px'
+  },
+  downRatingButton: {
+    top: '10px'
+  },
   ratingButtonVertical: {
+    position: 'unset',
     display: 'block'
+  },
+  unavailable: {
+    visibility: 'hidden',
+    color: grey[500]
   }
 });
 
@@ -38,6 +54,10 @@ const RATE_FOOD_MUTATION = gql`
 `;
 
 function Food({ food, foodsQuery, myFood, myFoodsQuery, classes }) {
+  const isAuthenticated = useQuery(AUTHENTICATION_QUERY).data.isAuthenticated;
+  const rateFood = useMutation(RATE_FOOD_MUTATION);
+  const [isRatingLoading, setIsRatingLoading] = useState(false);
+
   const ratingClasses = classNames(classes.rating, {
     [classes.ratingLow]: food.totalRating < 0.49,
     [classes.ratingMedium]: food.totalRating >= 0.49 && food.totalRating <= 0.51,
@@ -47,19 +67,12 @@ function Food({ food, foodsQuery, myFood, myFoodsQuery, classes }) {
     [classes.ratingButtonVertical]:
       typeof food.totalRating === 'undefined' || food.totalRating === null
   });
-
-  const isAuthenticated = useQuery(AUTHENTICATION_QUERY).data.isAuthenticated;
-  const rateFood = useMutation(RATE_FOOD_MUTATION);
-
-  const showUpIcon =
-    isAuthenticated &&
-    (!(foodsQuery.loading || myFoodsQuery.loading) &&
-      (!myFood || (myFood && myFood.myRating !== 1)));
-
-  const showDownIcon =
-    isAuthenticated &&
-    (!(foodsQuery.loading || myFoodsQuery.loading) &&
-      (!myFood || (myFood && myFood.myRating !== 0)));
+  const upRatingButtonClasses = classNames(ratingButtonClasses, classes.upRatingButton, {
+    [classes.unavailable]: !myFoodsQuery.loading && myFood && myFood.myRating === 1
+  });
+  const downRatingButtonClasses = classNames(ratingButtonClasses, classes.downRatingButton, {
+    [classes.unavailable]: !myFoodsQuery.loading && myFood && myFood.myRating === 0
+  });
 
   return (
     <TableRow>
@@ -67,48 +80,58 @@ function Food({ food, foodsQuery, myFood, myFoodsQuery, classes }) {
       <TableCell>
         {food.histamineLevel.value - 1} - {food.histamineLevel.name}
       </TableCell>
-      <TableCell>
-        {showUpIcon && (
-          <UpIcon
-            className={ratingButtonClasses}
-            onClick={() =>
-              rateFood({
-                variables: {
-                  foodId: food.id,
-                  value: 1
-                }
-              }).then(() => {
-                foodsQuery.refetch();
-                myFoodsQuery.refetch();
-              })
-            }
-          />
-        )}
-        {(food.totalRating || food.totalRating === 0) && (
+      <TableCell className={classes.ratingCell}>
+        {!isRatingLoading ? (
           <Fragment>
-            <Typography variant="h6" component="span" className={ratingClasses}>
-              {Math.round(food.totalRating * 10000) / 100}
-            </Typography>
-            <Typography variant="body2" component="span" className={ratingClasses}>
-              %
-            </Typography>
+            {isAuthenticated && (
+              <UpIcon
+                className={upRatingButtonClasses}
+                onClick={() => {
+                  setIsRatingLoading(true);
+                  rateFood({
+                    variables: {
+                      foodId: food.id,
+                      value: 1
+                    }
+                  }).then(() => {
+                    Promise.all([foodsQuery.refetch(), myFoodsQuery.refetch()]).then(() =>
+                      setIsRatingLoading(false)
+                    );
+                  });
+                }}
+              />
+            )}
+            {(food.totalRating || food.totalRating === 0) && (
+              <Fragment>
+                <Typography variant="h6" component="span" className={ratingClasses}>
+                  {Math.round(food.totalRating * 10000) / 100}
+                </Typography>
+                <Typography variant="body2" component="span" className={ratingClasses}>
+                  %
+                </Typography>
+              </Fragment>
+            )}
+            {isAuthenticated && (
+              <DownIcon
+                className={downRatingButtonClasses}
+                onClick={() => {
+                  setIsRatingLoading(true);
+                  rateFood({
+                    variables: {
+                      foodId: food.id,
+                      value: 0
+                    }
+                  }).then(() => {
+                    Promise.all([foodsQuery.refetch(), myFoodsQuery.refetch()]).then(() =>
+                      setIsRatingLoading(false)
+                    );
+                  });
+                }}
+              />
+            )}
           </Fragment>
-        )}
-        {showDownIcon && (
-          <DownIcon
-            className={ratingButtonClasses}
-            onClick={() =>
-              rateFood({
-                variables: {
-                  foodId: food.id,
-                  value: 0
-                }
-              }).then(() => {
-                foodsQuery.refetch();
-                myFoodsQuery.refetch();
-              })
-            }
-          />
+        ) : (
+          <CircularProgress size={24} thickness={6} color="primary" />
         )}
       </TableCell>
       <TableCell>{food.description}</TableCell>
