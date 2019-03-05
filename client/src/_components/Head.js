7f90bb0be02a9811432068cc2b15e1db46f7cf03
franzp <pobocekfrantisek@gmail.com>
2019-03-05 19:11:25 +0100

    Client - Head meat tags

--- client/src/_components/Head.js
+++ client/src/_components/Head.js
@@ -7,7 +7,13 @@ function Head() {
       <title>Bezhistaminovo</title>
       <meta
         name="description"
-        content="Bezhistaminovo je stranka pomahajuca ludom s histaminovou intoleranciou, kde mozu ludia hlasovat za jedla a recepty ci neuskodili"
+        content="Bezhistaminovo je stranka pomahajuca ludom s histaminovou intoleranciou, kde mozu ludia hlasovat za jedla a recepty."
+      />
+      <meta name="keywords" content="histamin, bezhistaminovo, recepty, potraviny, alergie" />
+      <meta name="og:url" content={window.location.origin} />
+      <meta
+        name="og:description"
+        content="Bezhistaminovo je stranka pomahajuca ludom s histaminovou intoleranciou, kde mozu ludia hlasovat za jedla a recepty."
       />
     </Helmet>
   );
